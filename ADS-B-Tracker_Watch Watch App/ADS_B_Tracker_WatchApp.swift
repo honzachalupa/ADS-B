c2b67ecf0f26fe62cb22b93f0181e0953398a92d
Jan Chalupa <janchalupa@outlook.cz>
2025-04-14 23:16:27 +0200

    test

---
+++ ADS-B-Tracker_Watch Watch App/ADS_B_Tracker_WatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  ADS_B_Tracker_WatchApp.swift
+//  ADS-B-Tracker_Watch Watch App
+//
+//  Created by Jan Chalupa on 14.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct ADS_B_Tracker_Watch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
