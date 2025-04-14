7638085a21c611007a83c07468b6d9b12f6bc480
Jan Chalupa <janchalupa@outlook.cz>
2025-04-14 22:51:54 +0200

    Revert "test"
    This reverts commit 993c039fe92f769018017f97e01f17c8c148d05e.

---
+++ ADS-B-Tracker/ADS_B_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ADS_B_TrackerApp.swift
+//  ADS-B-Tracker
+//
+//  Created by Jan Chalupa on 14.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct ADS_B_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
