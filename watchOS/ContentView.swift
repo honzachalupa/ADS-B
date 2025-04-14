993c039fe92f769018017f97e01f17c8c148d05e
Jan Chalupa <janchalupa@outlook.cz>
2025-04-14 22:45:38 +0200

    test

---
+++ watchOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ADS-B-Tracker_Watch Watch App
+//
+//  Created by Jan Chalupa on 14.04.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ watchOS/watchOS.swift
@@ -0,0 +1,10 @@
+import SwiftUI
+
+@main
+struct watchOS: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
