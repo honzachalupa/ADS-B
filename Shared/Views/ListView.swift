c10546e8b019cb6a88e257fc7d81142f93b0cd9d
Jan Chalupa <janchalupa@outlook.cz>
2025-11-04 19:22:52 +0100

    Updated SwiftCore dependency to version 1.8.2

--- Shared/Views/ListView.swift
+++ Shared/Views/ListView.swift
@@ -1,4 +1,5 @@
 import SwiftUI
+import SwiftCore
 
 struct ListView: View {
     @Binding public var selectedAircraft: Aircraft?
@@ -16,42 +17,41 @@ struct ListView: View {
     
     var body: some View {
         NavigationStack {
-            List(aircraftFiltered) { aircraft in
-                HStack {
-                    VStack(alignment: .leading) {
-                        Text(aircraft.flight?.trimmingCharacters(in: .whitespacesAndNewlines) ?? "-")
-                            .font(.headline)
-                        
-                        Text(aircraft.hex.uppercased())
-                            .font(.subheadline)
-                            .foregroundColor(.secondary)
-                    }
-                    
-                    Spacer()
-                    
-                    VStack(alignment: .trailing) {
-                        if let altitude = aircraft.alt_baro {
-                            Text(formatAltitude(altitude))
+            BackgroundGradient { isBackgroundGradientEnabled in
+                List(aircraftFiltered) { aircraft in
+                    HStack {
+                        VStack(alignment: .leading) {
+                            Text(aircraft.flight?.trimmingCharacters(in: .whitespacesAndNewlines) ?? "-")
+                                .font(.headline)
+                            
+                            Text(aircraft.hex.uppercased())
                                 .font(.subheadline)
+                                .foregroundColor(.secondary)
                         }
                         
-                        if let speed = aircraft.gs {
-                            Text(formatSpeed(speed))
-                                .font(.subheadline)
+                        Spacer()
+                        
+                        VStack(alignment: .trailing) {
+                            if let altitude = aircraft.alt_baro {
+                                Text(formatAltitude(altitude))
+                                    .font(.subheadline)
+                            }
+                            
+                            if let speed = aircraft.gs {
+                                Text(formatSpeed(speed))
+                                    .font(.subheadline)
+                            }
                         }
                     }
+                    .contentShape(Rectangle())
+                    .onTapGesture {
+                        selectedAircraft = aircraft
+                    }
+                    .searchable(text: $searchText, prompt: "Search...")
                 }
-                .contentShape(Rectangle())
-                .onTapGesture {
-                    selectedAircraft = aircraft
-                }
-                .searchable(text: $searchText, prompt: "Search...")
+                .scrollContentBackground(isBackgroundGradientEnabled ? .hidden : .automatic)
             }
-            .toolbar {
-                /* if #available(iOS 26, *) {
-                    DefaultToolbarItem(kind: .search, placement: .bottomBar)
-                } */
-            }
+            .navigationTitle("List")
         }
     }
 }
